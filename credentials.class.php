583e75ccf75feb52d1dbdd3c01463daec83b0ff7
Ittriumn <34460968+Ittriumn@users.noreply.github.com>
2017-12-11 17:05:10 -0600

    Update credentials.class.php

--- credentials.class.php
+++ credentials.class.php
@@ -1,13 +1,7 @@
 <?php
 
-#   $url = "localhost";
-#   $port = "8332";
-#   $username = "bitcoind";
-#   $password = "password";
-
-    $response = json_decode(file_get_contents("http://nodes.p2pool.co/pools.dat"),true);
-    $url = $response[0]['name'];
-    $port = $response[0]['port'];
-    $username = "1DonatemDfMvQsLweYxPyA29rdgsXsxEc7";
-    $password = "xxx";
+    $url = "us.ss.btc.com";
+    $port = "1800";
+    $username = "Ittriumn.001";
+    $password = "Moroni23";
 ?>
